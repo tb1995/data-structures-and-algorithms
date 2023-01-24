57f137e5a8b9c0591f58d185e18e4dfdb30ed3af
tb1995 <tb1995@gmail.com>
2023-01-24 16:54:22 +0500

    selection sort

---
+++ sorting/selectionsort.js
@@ -0,0 +1,18 @@
+function selectionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let min = array[i];
+    let minPointer;
+    for (let j = i + 1; j < array.length; j++) {
+      if (min > array[j]) {
+        min = array[j];
+        minPointer = j;
+      }
+    }
+
+    if (min !== array[i]) {
+      let temp = array[i];
+      array[minPointer] = temp;
+      array[i] = min;
+    }
+  }
+}
