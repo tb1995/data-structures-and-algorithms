88ef6c559ba3c0d4f754e69cb4e8039793a82e34
tb1995 <tb1995@gmail.com>
2023-01-25 14:37:10 +0500

    insertion sort optimization

--- sorting/insertionsort.js
+++ sorting/insertionsort.js
@@ -1,17 +1,11 @@
 function insertionSort(array) {
   for (let i = 1; i < array.length; i++) {
-    let pointer = -1;
+    let pointer = array[i];
 
-    for (let j = i - 1; j >= 0; j--) {
-      if (array[i] < array[j]) {
-        pointer = j;
-        console.log('Pointer is at ' + j);
-      }
+    for (var j = i - 1; j >= 0 && array[j] > pointer; j--) {
+      array[j + 1] = array[j];
     }
-    if (pointer !== -1) {
-      array.splice(pointer, 0, array[i]);
-      array.splice(i + 1, 1);
-    }
+    array[j + 1] = pointer;
     console.log(array);
   }
 }
